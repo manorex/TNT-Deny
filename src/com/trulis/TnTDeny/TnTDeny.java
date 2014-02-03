2a603c6ce9db2c96e04c2c7deae3479a593a38fb
H3adCracker <themanor3x@gmail.com>
2014-02-03 15:57:30 +0200

    Source Update

--- src/com/trulis/TnTDeny/TnTDeny.java
+++ src/com/trulis/TnTDeny/TnTDeny.java
@@ -7,30 +7,31 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TnTDeny extends JavaPlugin {
-
+public class TnTDeny extends JavaPlugin{
+	
 	public final Logger logger = Logger.getLogger("Minecraft");
 	public static TnTDeny plugin;
 	public final TrulisBlockListener bl = new TrulisBlockListener();
-
-	public void onDisable() {
+	
+	
+	@Override
+	public void onDisable(){
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion()
-				+ " Has Been Disabled!");
+		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Disabled!");
 	}
 
-	public void onEnable() {
+	@Override
+	public void onEnable(){
 		PluginDescriptionFile pdfFile = this.getDescription();
-		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion()
-				+ " Has Been Enabled!");
+		this.logger.info(pdfFile.getName() + " Version " + pdfFile.getVersion() + " Has Been Enabled!");
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(this.bl, this);
-
+		
 		try {
-			Metrics metrics = new Metrics(this);
-			metrics.start();
+		    Metrics metrics = new Metrics(this);
+		    metrics.start();
 		} catch (IOException e) {
-			// Failed to submit the stats :-(
+		    // Failed to submit the stats :-(
 		}
 	}
 
